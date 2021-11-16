de53cace69fca242d19a7478ebac533ece21e5a7
David Storey <davestorey19@hotmail.com>
2021-11-16 22:13:18 +0000

    updated with man utd players

--- player_wisdom.js
+++ player_wisdom.js
@@ -1,12 +1,39 @@
-var items = ['Stam', 'Campbell', 'Vidic'];
-var first_item = items[Math.floor(Math.random() * items.length)];
+// Object to contain a selection of Man Utd players stored by position
+const manUtdPlayers = {
+    goalKeepers: ['Schmeichel', "van der Gouw", "Bosnic", "van der Sar"],
+    defenders: ["Pallister", "Bruce", "Stam", "Ferdinand"],
+    midfielder1: ["Keane", "Scholes", "Butt"],
+    midfielder2: ["Beckham", "Giggs", "Veron"],
+    forward: ["van Nistelrooy", "Cole", "Yorke", "Solskjaer"]
+};
+// empty array to holds results
+let team = [];
 
-var items2 = ['Beckham', 'Giggs', 'Scholes'];
-var second_item = items2[Math.floor(Math.random() * items.length)];
+// random number generator to use with for in loop
+let randomSelect = arrayLength => {
+	return Math.floor(Math.random() * arrayLength);
+}
 
-var items3 = ['Cole', 'Yorke', 'van Nistelrooy'];
-var third_item = items3[Math.floor(Math.random() * items.length)];
-
-let combined = (`The defender is: ${first_item}; The midfielder is: ${second_item}; The striker is: ${third_item}`);
-
-console.log(combined);
+// for in loop with switch statement that pushes result of each case (position) to the team array
+for(let property in manUtdPlayers){
+    let randomIndex = randomSelect(manUtdPlayers[property].length);
+    switch (property) {
+        case 'goalKeepers':
+            team.push(manUtdPlayers[property][randomIndex]);
+            break;
+        case 'defenders':
+            team.push(manUtdPlayers[property][randomIndex]);
+            break;
+        case 'midfielder1':
+            team.push(manUtdPlayers[property][randomIndex]);
+            break;
+        case 'midfielder2':
+            team.push(manUtdPlayers[property][randomIndex]);
+            break;
+        case 'forward':
+            team.push(manUtdPlayers[property][randomIndex]);
+            break;
+    }
+}
+console.log(team.join(', '));
+  
